e36a52f2e773a7fd7a2d31a08b89ad0ca062c102
Denislav Zaimov <dzaimov@Denislavs-MBP.home>
2019-12-16 21:11:10 -0500

    removing libsass packages and sass_processor and related code

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sass_processor',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,11 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
-]
-
-# Django Sass
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
