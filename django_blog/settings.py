95ff7fd022e0a9a660ab243073f0b46137896278
denis <denis.zaimov@martindale.com>
2019-12-19 16:40:31 -0500

    created users app and started building registration form functionality

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
+    'blog',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
